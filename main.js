622c60f16996a2b30af4b482bc53938cf6892656
Douglas <douglasscottjohnston@gmail.com>
2023-01-09 11:31:52 -0800

    put a ball on the screen

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const gameEngine = new GameEngine();
 //Austin
 const ASSET_MANAGER = new AssetManager();
-
+ASSET_MANAGER.queueDownload("./assets/circlePixel.png");
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
-
+	gameEngine.addEntity(new Player(gameEngine));
 	gameEngine.init(ctx);
 
 	gameEngine.start();
