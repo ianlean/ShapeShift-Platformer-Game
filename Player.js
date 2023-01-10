ba999da41ed03014c5412e3259538c0370dd63b4
not-damien <93546644+not-damien@users.noreply.github.com>
2023-01-10 13:40:54 -0800

    added clause for shift key

--- Player.js
+++ Player.js
@@ -10,8 +10,9 @@ class Player {
         const Acceleration = 1;
         this.velocityX = 0;
         this.velocityY = 0;
+        this.spritesheet = ASSET_MANAGER.getAsset("./assets/circlePixel.png")
     };
-
+    
     update() {
         if(this.game.keys["d"] == true){
             this.velocityX = 1
@@ -25,13 +26,21 @@ class Player {
         if(this.game.keys["s"] == true){
             this.velocityY = 1
         }
+        if(this.game.keys["Shift"]== true){ 
+            this.spritesheet = ASSET_MANAGER.getAsset("./assets/sqaurePixel.png")
+            console.log("changing shape")
+            //this should probably get pulled into it own function with some kind of way to rotate between all shapes 
+        }
 
         this.x += this.velocityX;
         this.y += this.velocityY;
+        //gravity and other thing effecting movement could go here
     };
 
     draw(ctx){
-        ctx.drawImage(ASSET_MANAGER.getAsset("./assets/circlePixel.png"), this.x, this.y);
+        ctx.drawImage(this.spritesheet, this.x, this.y);
     };
+
+    
 
 }
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const gameEngine = new GameEngine();
 //Austin
 const ASSET_MANAGER = new AssetManager();
 ASSET_MANAGER.queueDownload("./assets/circlePixel.png");
+ASSET_MANAGER.queueDownload("./assets/sqaurePixel.png");
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
