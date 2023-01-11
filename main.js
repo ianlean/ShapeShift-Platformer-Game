761a5226dd50036a6c853248633d6824b757db0d
Tinytin06 <52684983+Tinytin06@users.noreply.github.com>
2023-01-11 12:42:07 -0800

    main

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const gameEngine = new GameEngine();
 const ASSET_MANAGER = new AssetManager();
 ASSET_MANAGER.queueDownload("./assets/circlePixel.png");
 ASSET_MANAGER.queueDownload("./assets/sqaurePixel.png");
+ASSET_MANAGER.queueDownload("./assets/trianglePixel.png");
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
