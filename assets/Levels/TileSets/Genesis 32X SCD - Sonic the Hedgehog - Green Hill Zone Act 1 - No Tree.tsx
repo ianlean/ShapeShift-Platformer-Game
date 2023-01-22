6eabce771c241eeea8247b2d5e47d72249e78799
Douglas <douglasscottjohnston@gmail.com>
2023-01-21 17:22:22 -0800

    created a simple level

---
+++ assets/Levels/TileSets/Genesis 32X SCD - Sonic the Hedgehog - Green Hill Zone Act 1 - No Tree.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Genesis 32X SCD - Sonic the Hedgehog - Green Hill Zone Act 1 - No Tree" tilewidth="32" tileheight="32" tilecount="15408" columns="321">
+ <image source="Genesis 32X SCD - Sonic the Hedgehog - Green Hill Zone Act 1 - No Tree.png" trans="871013" width="10288" height="1566"/>
+</tileset>
