bd7350e57feb331276a4bbbecb3abcb8f12cbecb
ianlean <ianlean717@gmail.com>
2023-01-31 11:10:08 -0800

    added collision between circle

--- BoundingCircle.js
+++ BoundingCircle.js
@@ -3,11 +3,30 @@ class BoundingCircle {
         Object.assign(this, { x, y, radius });
     };
 
-    collide(oth) {//logic still needs to be written
-        return false;
-    };
     //for debugging purposes
-    draw(ctx){
-        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
+    draw(ctx) {
+        ctx.strokeStyle = "Red";
+        ctx.lineWidth = 10;
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
     }
-}
+
+    update() {
+
+    }
+
+    collide(oth) {
+        //compare the distance to combined radii
+        if (oth != undefined) {
+            var dx = oth.x - this.x;
+            var dy = oth.y - this.y;
+            var radii = oth.radius + this.radius;
+            if ( ( dx * dx )  + ( dy * dy ) < radii * radii ) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+};
