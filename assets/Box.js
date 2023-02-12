355faaa1a804cb9ea10affe587c17c91ac828a9a
Tinytin06 <52684983+Tinytin06@users.noreply.github.com>
2023-02-12 14:18:35 -0800

    made box class

---
+++ assets/Box.js
@@ -0,0 +1,22 @@
+class Box {
+    //
+    constructor(game, x, y, width, height) {
+        this.game = game;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.BoundingBox = new BoundingBox(this.x, this.y, this.width, this.height);
+    };
+    update() {
+
+    };
+    draw(ctx) {
+        this.BoundingBox.draw(ctx);
+    };
+    updateCollision() {
+        this.BoundingBox = new BoundingBox(this.x, this.y, this.width, this.height);
+    }
+
+
+}
