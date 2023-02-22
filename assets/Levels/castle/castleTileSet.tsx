e50ea77a041ec09a9b5b747312d65b64c99bdcf1
not-damien <93546644+not-damien@users.noreply.github.com>
2023-02-21 23:04:14 -0800

    fixed tilesets

--- assets/Levels/castle/castleTileSet.tsx
+++ assets/Levels/castle/castleTileSet.tsx
@@ -2,33 +2,33 @@
 <tileset version="1.9" tiledversion="1.9.2" name="castleTileSet" tilewidth="166" tileheight="64" tilecount="10" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="64" height="32" source="../../../../../paint playground/arch.png"/>
+  <image width="64" height="32" source="../../../../../tileSinglepics/arch.png"/>
  </tile>
  <tile id="1">
-  <image width="64" height="32" source="../../../../../paint playground/brick1.png"/>
+  <image width="64" height="32" source="../../../../../tileSinglepics/brick1.png"/>
  </tile>
  <tile id="2">
-  <image width="32" height="32" source="../../../../../paint playground/brick2.png"/>
+  <image width="32" height="32" source="../../../../../tileSinglepics/brick2.png"/>
  </tile>
  <tile id="3">
-  <image width="161" height="32" source="../../../../../paint playground/brick3.png"/>
+  <image width="161" height="32" source="../../../../../tileSinglepics/brick3.png"/>
  </tile>
  <tile id="4">
-  <image width="64" height="32" source="../../../../../paint playground/brick4.png"/>
+  <image width="64" height="32" source="../../../../../tileSinglepics/brick4.png"/>
  </tile>
  <tile id="5">
-  <image width="64" height="64" source="../../../../../paint playground/bigbrick1.png"/>
+  <image width="64" height="64" source="../../../../../tileSinglepics/bigbrick1.png"/>
  </tile>
  <tile id="6">
-  <image width="32" height="16" source="../../../../../paint playground/smol1.png"/>
+  <image width="32" height="16" source="../../../../../tileSinglepics/smol1.png"/>
  </tile>
  <tile id="7">
-  <image width="32" height="24" source="../../../../../paint playground/chestClosed.png"/>
+  <image width="32" height="24" source="../../../../../tileSinglepics/chestClosed.png"/>
  </tile>
  <tile id="8">
-  <image width="166" height="45" source="../../../../../paint playground/cloud1.png"/>
+  <image width="166" height="45" source="../../../../../tileSinglepics/cloud1.png"/>
  </tile>
  <tile id="9">
-  <image width="133" height="43" source="../../../../../paint playground/cloud2.png"/>
+  <image width="133" height="43" source="../../../../../tileSinglepics/cloud2.png"/>
  </tile>
 </tileset>
--- assets/Levels/levelDC/DamenTestSet.tsx
+++ assets/Levels/levelDC/DamenTestSet.tsx
@@ -2,18 +2,18 @@
 <tileset version="1.9" tiledversion="1.9.2" name="DamenTestSet" tilewidth="336" tileheight="102" tilecount="5" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="336" height="79" source="../../../../../paint playground/one.png"/>
+  <image width="336" height="79" source="../../../../../tileSinglepics/one.png"/>
  </tile>
  <tile id="1">
-  <image width="137" height="58" source="../../../../../paint playground/two.png"/>
+  <image width="137" height="58" source="../../../../../tileSinglepics/two.png"/>
  </tile>
  <tile id="2">
-  <image width="95" height="46" source="../../../../../paint playground/three.png"/>
+  <image width="30" height="15" source="../../../../../tileSinglepics/three.png"/>
  </tile>
  <tile id="3">
-  <image width="107" height="102" source="../../../../../paint playground/four.png"/>
+  <image width="107" height="102" source="../../../../../tileSinglepics/four.png"/>
  </tile>
  <tile id="4">
-  <image width="86" height="78" source="../../../../../paint playground/dirt.png"/>
+  <image width="86" height="78" source="../../../../../tileSinglepics/dirt.png"/>
  </tile>
 </tileset>
