cd13dd5f340bf3aecc55d2306b52d4c3fa3b3f48
ianlean <ianlean717@gmail.com>
2023-03-04 09:25:29 -0800

    adding some levels

---
+++ assets/Levels/ian_level/space_level.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="space_level" tilewidth="64" tileheight="73" tilecount="11" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="7">
+  <image width="64" height="65" source="1.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="65" source="2.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="63" source="3.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="63" source="4.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="55" source="5.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="73" source="6.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="63" source="7.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="Screenshot 2023-03-03 at 9.35.46 PM.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="62" source="Screenshot 2023-03-03 at 9.36.01 PM.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="63" source="Screenshot 2023-03-03 at 9.36.15 PM.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="6" source="Screenshot 2023-03-03 at 9.42.57 PM.png"/>
+ </tile>
+</tileset>
