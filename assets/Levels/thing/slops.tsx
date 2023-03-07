aee4568c4fc662f270d1de6f5d18ceb0e81e8b0e
not-damien <93546644+not-damien@users.noreply.github.com>
2023-03-07 00:05:52 -0800

    fixing ians bullshit

--- assets/Levels/thing/slops.tsx
+++ assets/Levels/thing/slops.tsx
@@ -1,10 +1,22 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="slops" tilewidth="134" tileheight="132" tilecount="2" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="slops" tilewidth="134" tileheight="132" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="134" height="132" source="Screenshot 2023-03-06 at 5.29.03 PM.png"/>
  </tile>
- <tile id="19">
-  <image width="64" height="32" source="Spikes.png"/>
+ <tile id="21">
+  <image width="64" height="64" source="Spikes.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="Spikes_short.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="64" source="Springy.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="64" source="bigger13.png"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="64" source="spikes3.png"/>
  </tile>
 </tileset>
